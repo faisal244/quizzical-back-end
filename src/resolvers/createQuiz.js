1e56331214ba49ddfe4f6655c59c5d5a3e8c382f
Faisal Mohammed <m.faisal244@gmail.com>
2022-09-12 17:59:04 +0100

    Added title to quiz schema, models and resolvers

--- src/resolvers/createQuiz.js
+++ src/resolvers/createQuiz.js
@@ -4,30 +4,31 @@ const Quiz = require("../models/Quiz.js");
 const User = require("../models/User.js");
 
 const creatingQuiz = async (_, { createQuizInput }, { user }) => {
-  try {
-    if (!user) {
-      return new AuthenticationError("You are not Authorised Buddy Try Again!");
-    }
+	try {
+		if (!user) {
+			return new AuthenticationError("You are not Authorised Buddy Try Again!");
+		}
 
-    // destructurer the create quiz args object
+		// destructurer the create quiz args object
 
-    const { category, difficulty, type, questions } = createQuizInput;
-    // save quiz in DB using Quiz model
+		const { category, difficulty, type, questions, title } = createQuizInput;
+		// save quiz in DB using Quiz model
 
-    const newQuiz = await Quiz.create({
-      category,
-      difficulty,
-      type,
-      questions,
-    });
+		const newQuiz = await Quiz.create({
+			category,
+			title,
+			difficulty,
+			type,
+			questions,
+		});
 
-    await User.findByIdAndUpdate(user.id, { $push: { quizzes: newQuiz._id } });
+		await User.findByIdAndUpdate(user.id, { $push: { quizzes: newQuiz._id } });
 
-    return newQuiz;
-  } catch (error) {
-    console.log(error.message);
-    return new ApolloError("Failed to create quiz");
-  }
+		return newQuiz;
+	} catch (error) {
+		console.log(error.message);
+		return new ApolloError("Failed to create quiz");
+	}
 };
 
 module.exports = creatingQuiz;
